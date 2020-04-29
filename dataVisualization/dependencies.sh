ab552d3fcd5c61586cd341118afe3d44fcd27de7
KodyJ97 <kkjohns9@asu.edu>
2020-04-29 16:44:34 -0700

    new Dependency

--- dataVisualization/dependencies.sh
+++ dataVisualization/dependencies.sh
@@ -3,3 +3,4 @@ sudo apt-get install python3-matplotlib
 apt-get install python3.7-dev python3.7-tk tk-dev
 apt-get install libfreetype6-dev g++
 pip3 install --user matplotlib
+pip3 install --user pygal
