981dad68a7b9308bdf73a178e79ba820ece9978f
KodyJ97 <kkjohns9@asu.edu>
2020-05-10 16:16:49 -0700

    New dependency and start of world pop proj

--- dataVisualization/dependencies.sh
+++ dataVisualization/dependencies.sh
@@ -2,5 +2,6 @@
 sudo apt-get install python3-matplotlib
 apt-get install python3.7-dev python3.7-tk tk-dev
 apt-get install libfreetype6-dev g++
-pip3 install --user matplotlib
-pip3 install --user pygal
+pip3 install matplotlib
+pip3 install pygal
+pip3 install pygal_maps_world
