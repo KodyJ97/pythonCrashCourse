964ca1ae263648e15bf6864c6a47a86a00cf36f8
KodyJ97 <kkjohns9@asu.edu>
2020-05-14 15:59:43 -0700

    New app for users Later with implement registration

---
+++ learnDjango/learning_log/users/urls.py
@@ -0,0 +1,16 @@
+"""Defines URL patterns for user"""
+
+from django.urls import path
+from django.contrib.auth import views as auth_views
+
+from . import views
+
+app_name = 'users'
+
+urlpatterns = [
+    # Login page
+    path('login', auth_views.LoginView.as_view(template_name='users/login.html'),
+    name='login'),
+    # Logout page
+    path('logout/', views.logout_view, name='logout'),
+]
